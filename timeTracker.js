c73fc9fae26f72ac0c6b85d747fe4fcde17439bb
J'son David <jdavid@TD4D6.WPA.Dal.Ca>
2024-12-03 13:58:06 -0400

    Checking if the password and emails are the right ones

--- timeTracker.js
+++ timeTracker.js
@@ -2,10 +2,9 @@ function returnText() {
     let username = document.getElementById(`username`).value;
     let password = document.getElementById(`password`).value;
     if (username == ``) {
-        alert(`Please enter a username`)
+        
     }
     if (password == ``) {
-        alert(`Please enter your password`)
     }
     if (condition) {
         
