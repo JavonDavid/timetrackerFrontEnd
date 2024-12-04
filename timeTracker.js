435c7d2fab6aa5aa4a3c91b0d7b1133341a00480
J'son David <jdavid@TD4BF.WPA.Dal.Ca>
2024-12-04 14:25:58 -0400

    Trying to use axios and seeing how it works

--- timeTracker.js
+++ timeTracker.js
@@ -1,23 +1,33 @@
-const axios = require("axios");
-axios.defaults.baseURL = "http://localhost:3000/";
-/* checks user input and if the email and password inputs are empty */
-function returnText() {
-  /* gets the value of the email and password from inputs */
-  const email = document.getElementById(`email`).value.trim();
-  const password = document.getElementById(`password`).value.trim();
-  /* checking if the user left email and password empty */
-  if (!email || !password) {
-    return console.log("User has not enter there email or password");
-  }
-}
+import axios, { defaults, get } from "axios";
+const baseURL = defaults.baseURL = "http://localhost:3000/";
+const id = defaults.baseURL = `http://localhost:3000/user/:id`;
 /* fetching the  users and logging and log them in the console if data has been fetched or a error has happend*/
 async function fecthUsers() {
   try {
-    const responce = await axios.get(`http://localhost:3000/users`);
+    const responce = await axios.get(baseURL);
     console.log(`Users fetched: `, responce.data);
   } catch (error) {
-    console.error(`Error fetching users: `, error.massage);
+    console.error(`Error fetching users: `, error.responce? error.responce.data : error.massage);
   }
 }
+async function getUsersById(id){
+try {
+  const responce = await axios.get(`${baseURL}/${id}`);
+  console.log(`User found by id`, responce.data);
+}
+catch (error) {
+  console.error(`Error finding user by id`, error.responce? error.responce.data : error.massage);
+}
+}
+async function updateUser(id){
+  try {
+    const updateUser = {email:`javon1234@gmail.com`, password:`notsosmallman1234`};
+    const responce = await axios.put(`http://localhost:3000/user/:id`,updateUser);
+    console.log(`User has been updated`, responce.data); 
+  } catch (error) {
+    console.error('Error updating user',error.responce? error.responce.data : error.massage);
+  }
+}
+window.getUsersById= getUsersById;
 window.fecthUsers = fecthUsers;
-window.returnText = returnText;
+window.updateUser= updateUser;
